77325899e8874c4f31568fac535fa56d4b9a6fee
shirow.ozawa <shiro.ozawa@2nd-community.com>
2022-06-23 18:33:46 +0900

    refactor:読み込み対象とするシート名を変数化した

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def main():
   todayDate = datetime.now().strftime("%Y%m%d")
   templateFilePath = './template/YYYYMMDD_業務日報_.xlsx'
   distFilePath = os.path.join(dist, f"{ todayDate }_業務日報_.xlsx")
+  targetSheet = '業務報告書'
 
   # template からコピーして新規ファイル作成
   shutil.copyfile(templateFilePath, distFilePath)
 
   wb = openpyxl.load_workbook(distFilePath)
-  sheet = wb['業務報告書']
+  sheet = wb[targetSheet]
 
   # 読み込み: OK
   value = sheet.cell(row=1, column=1).value
