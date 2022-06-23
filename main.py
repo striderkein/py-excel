2a65fbc441886e357eabe5c52e8b5fe8b1ac63c5
shirow.ozawa <shiro.ozawa@2nd-community.com>
2022-06-23 18:21:31 +0900

    fix:fix template path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 dist = os.path.expanduser('~/Documents/daily_repo/dist')
 todayDate = today = datetime.now().strftime("%Y%m%d")
-templateFilePath = '../../template/YYYYMMDD_業務日報_.xlsx'
+templateFilePath = './template/YYYYMMDD_業務日報_.xlsx'
 distFilePath = os.path.join(dist, f"{ todayDate }_業務日報_.xlsx")
 
 # template からコピーして新規ファイル作成
