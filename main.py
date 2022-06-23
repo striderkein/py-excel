739f7e935e53166e7d268f150a08c607c895d0e7
shirow.ozawa <shiro.ozawa@2nd-community.com>
2022-06-23 18:27:47 +0900

    refactor:use main
    for modulize and test

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 import shutil
 from datetime import datetime
 
-dist = os.path.expanduser('~/Documents/daily_repo/dist')
-todayDate = today = datetime.now().strftime("%Y%m%d")
-templateFilePath = './template/YYYYMMDD_業務日報_.xlsx'
-distFilePath = os.path.join(dist, f"{ todayDate }_業務日報_.xlsx")
+def main():
+  dist = os.path.expanduser('~/Documents/daily_repo/dist')
+  todayDate = today = datetime.now().strftime("%Y%m%d")
+  templateFilePath = './template/YYYYMMDD_業務日報_.xlsx'
+  distFilePath = os.path.join(dist, f"{ todayDate }_業務日報_.xlsx")
 
-# template からコピーして新規ファイル作成
-shutil.copyfile(templateFilePath, distFilePath)
+  # template からコピーして新規ファイル作成
+  shutil.copyfile(templateFilePath, distFilePath)
+
+if __name__ == '__main__':
+  main()
