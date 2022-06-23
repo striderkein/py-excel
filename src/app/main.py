088522e8c3c94d9b44354cc1bd5b8b72c3e250d2
shirow.ozawa <shiro.ozawa@2nd-community.com>
2022-06-23 16:52:03 +0900

    feat:テンプレートをコピーして任意のディレクトリにコピーできるようにした

---
+++ src/app/main.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+from datetime import datetime
+
+dist = os.path.expanduser('~/Documents/daily_repo/dist')
+todayDate = today = datetime.now().strftime("%Y%m%d")
+templateFilePath = '../../template/YYYYMMDD_業務日報_.xlsx'
+distFilePath = os.path.join(dist, f"{ todayDate }_業務日報_.xlsx")
+
+# template からコピーして新規ファイル作成
+shutil.copyfile(templateFilePath, distFilePath)
