a5ffe4fe32a8d72a7b47271c59bbc3d5699a0760
shirow.ozawa <shiro.ozawa@2nd-community.com>
2022-06-23 18:32:08 +0900

    feat:「指定したExcelファイルからの読み込み」を実装

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from datetime import datetime
+import openpyxl
 
 def main():
   dist = os.path.expanduser('~/Documents/daily_repo/dist')
@@ -10,6 +11,15 @@ def main():
 
   # template からコピーして新規ファイル作成
   shutil.copyfile(templateFilePath, distFilePath)
+
+  wb = openpyxl.load_workbook(distFilePath)
+  sheet = wb['業務報告書']
+
+  # 読み込み: OK
+  value = sheet.cell(row=1, column=1).value
+  print(value)
+
+  wb.close()
 
 if __name__ == '__main__':
   main()
