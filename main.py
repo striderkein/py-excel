6cb39dd6507d46b177b75417f52d7cede17063fa
shirow.ozawa <striderkein@users.noreply.github.com>
2022-06-24 23:15:43 +0900

    style:変数名の変更,コメントの追加と削除

--- main.py
+++ main.py
@@ -24,21 +24,22 @@ def main():
   wb = openpyxl.load_workbook(distFilePath)
   sheet = wb[targetSheet]
 
-
-  repo = 'art-lesson'
-  # command = f'cd ~/dev/{ repo };git log --oneline --branches --reverse --date=short --author=\"$(git config user.name)\" --pretty=format:\"- [%ad] %h: %s\" --since=\"last day\"'
-  command = f'cd ~/dev/{ repo };git log --oneline --branches --reverse --date=short --author=\"$(git config user.name)\" --pretty=format:\"%h,%s\" --since=\"last day\"'
-  stream = os.popen(command)
-  logs = stream.read().split('\n')
-
   cell = util.convCell("B1")
-  startCell = util.convCell("A6")
-  endCell = util.convCell("B6")
+  fromCell = util.convCell("A6")
+  toCell = util.convCell("B6")
   asanaTaskNameCell = util.convCell("D6")
   detailCell = util.convCell("E6")
   remarksCell = util.convCell("F6")
   startRow = detailCell['row']
 
+  repo = 'art-lesson'
+  asanaTaskName = 'some asana task'
+  # command = f'cd ~/dev/{ repo };git log --oneline --branches --reverse --date=short --author=\"$(git config user.name)\" --pretty=format:\"- [%ad] %h: %s\" --since=\"last day\"'
+  command = f'cd ~/dev/{ repo };git log --oneline --branches --reverse --date=short --author=\"$(git config user.name)\" --pretty=format:\"%h|%s\" --since=\"last day\"'
+  stream = os.popen(command)
+  logs = stream.read().split('\n')
+
+  # ヘッダ部分の出力
   # print(f"cell['row']:{ cell['row'] }, cell.['col']:{ cell['col'] }")
   # insert to cell: A1
   # op.1: with positional arguments
@@ -47,18 +48,22 @@ def main():
   # sheet.cell(1, 2).value = todayDateSeparateWithSlash
   sheet.cell(row=cell['row'], column=cell['col']).value = todayDateSeparateWithSlash
   sheet.cell(row=2, column=2).value = myName
-  sheet.cell(row=startCell['row'], column=startCell['col']).value = "10:00"
-  sheet.cell(row=endCell['row'], column=endCell['col']).value = "19:00"
-  sheet.cell(row=asanaTaskNameCell['row'], column=asanaTaskNameCell['col']).value = "some task"
+  sheet.cell(row=fromCell['row'], column=fromCell['col']).value = "10:00"
+  sheet.cell(row=toCell['row'], column=toCell['col']).value = "19:00"
+  sheet.cell(row=asanaTaskNameCell['row'], column=asanaTaskNameCell['col']).value = asanaTaskName
 
+  # テーブル部分の出力
   for i, log in enumerate(logs):
     print(f'{ log }')
-    lo = log.split(',')
+    lo = log.split('|')
     sheet.cell(row=startRow + i, column=detailCell['col']).value = lo[1]
     sheet.cell(row=startRow + i, column=remarksCell['col']).value = lo[0]
 
+  # 保存 → ブッククローズ
   wb.save(distFilePath)
   wb.close()
+
+  print('all done!')
 
 if __name__ == '__main__':
   main()
