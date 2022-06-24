3162a5b792b2ff1a2c811118209b2a9273fd37d5
shirow.ozawa <shiro.ozawa@2nd-community.com>
2022-06-24 15:51:36 +0900

    docs:modify comment

--- my/util.py
+++ my/util.py
@@ -21,10 +21,12 @@ def convCell(address: str) -> dict:
     { row: 行番号を表す数値, col: 列番号を表す数値 }
   """
   converted = dict()
-  # converted = None
-  # if type(address) == List[int]:
-  # 'A' -> 65 -> 1 となる変換
+
+  # 引数の1文字めについて下記の処理を行い、'A' -> 65 -> 1 となる変換を実現する。
+  # 1 Unicode コードポイントを表す整数に変換
+  # 2. 1 の処理結果に対して 64 を減じる
   converted['col'] = ord(address[0:1]) - 64
+  # 引数の2文字めは int への変換のみ
   converted['row'] = int(address[1:2])
   return converted
 
