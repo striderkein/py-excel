bef541ab0a49f5da2e1af8db431fe6c56bc08c9c
shirow.ozawa <striderkein@users.noreply.github.com>
2022-06-24 22:52:26 +0900

    feat:git logして複数行に出力することができるようにした
    リポジトリ切り替えは未対応

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ def main():
   load_dotenv()
   myName = os.getenv('CURRENT_USER_NAME_JP')
 
-  # print(util.convCell(List[1, 2])) # NG
-
   dist = os.path.expanduser('~/Documents/daily_repo/dist')
   todayDate = datetime.now().strftime("%Y%m%d")
   todayDateSeparateWithSlash = datetime.now().strftime("%Y/%m/%d")
@@ -22,15 +20,16 @@ def main():
 
   # template からコピーして新規ファイル作成
   shutil.copyfile(templateFilePath, distFilePath)
-
   # ディレクトリを指定してワークブック（いわゆる Excel ファイル）を開く
   wb = openpyxl.load_workbook(distFilePath)
   sheet = wb[targetSheet]
 
-  # for DEBUG
-  # 読み込み: OK
-  # value = sheet.cell(row=1, column=1).value
-  # print(value)
+
+  repo = 'art-lesson'
+  # command = f'cd ~/dev/{ repo };git log --oneline --branches --reverse --date=short --author=\"$(git config user.name)\" --pretty=format:\"- [%ad] %h: %s\" --since=\"last day\"'
+  command = f'cd ~/dev/{ repo };git log --oneline --branches --reverse --date=short --author=\"$(git config user.name)\" --pretty=format:\"%h,%s\" --since=\"last day\"'
+  stream = os.popen(command)
+  logs = stream.read().split('\n')
 
   cell = util.convCell("B1")
   startCell = util.convCell("A6")
@@ -38,7 +37,9 @@ def main():
   asanaTaskNameCell = util.convCell("D6")
   detailCell = util.convCell("E6")
   remarksCell = util.convCell("F6")
-  print(f"cell['row']:{ cell['row'] }, cell.['col']:{ cell['col'] }")
+  startRow = detailCell['row']
+
+  # print(f"cell['row']:{ cell['row'] }, cell.['col']:{ cell['col'] }")
   # insert to cell: A1
   # op.1: with positional arguments
   # sheet.cell(row=1, column=2).value = todayDateSeparateWithSlash
@@ -49,8 +50,12 @@ def main():
   sheet.cell(row=startCell['row'], column=startCell['col']).value = "10:00"
   sheet.cell(row=endCell['row'], column=endCell['col']).value = "19:00"
   sheet.cell(row=asanaTaskNameCell['row'], column=asanaTaskNameCell['col']).value = "some task"
-  sheet.cell(row=detailCell['row'], column=detailCell['col']).value = "working detail"
-  sheet.cell(row=remarksCell['row'], column=remarksCell['col']).value = "many bugs exists!"
+
+  for i, log in enumerate(logs):
+    print(f'{ log }')
+    lo = log.split(',')
+    sheet.cell(row=startRow + i, column=detailCell['col']).value = lo[1]
+    sheet.cell(row=startRow + i, column=remarksCell['col']).value = lo[0]
 
   wb.save(distFilePath)
   wb.close()
